816086027e6fa6aab9605ef6573c28f2ee850953
Chevano Simon <65667654+ChevanoSimon@users.noreply.github.com>
2020-06-18 11:45:55 +0200

    Add files via upload

---
+++ Qwixx/Qwixx/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Qwixx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btndobbelsteen = new System.Windows.Forms.Button();
+            this.imgWit1 = new System.Windows.Forms.PictureBox();
+            this.imgWit2 = new System.Windows.Forms.PictureBox();
+            this.imgGeel = new System.Windows.Forms.PictureBox();
+            this.imgGroen = new System.Windows.Forms.PictureBox();
+            this.imgRood = new System.Windows.Forms.PictureBox();
+            this.imgBlauw = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgWit1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgWit2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgGeel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgGroen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgRood)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBlauw)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btndobbelsteen
+            // 
+            this.btndobbelsteen.Location = new System.Drawing.Point(12, 626);
+            this.btndobbelsteen.Name = "btndobbelsteen";
+            this.btndobbelsteen.Size = new System.Drawing.Size(75, 23);
+            this.btndobbelsteen.TabIndex = 0;
+            this.btndobbelsteen.Text = "Dobbelsteen";
+            this.btndobbelsteen.UseVisualStyleBackColor = true;
+            this.btndobbelsteen.Click += new System.EventHandler(this.btndobbelsteen_Click);
+            // 
+            // imgWit1
+            // 
+            this.imgWit1.Location = new System.Drawing.Point(1060, 543);
+            this.imgWit1.Name = "imgWit1";
+            this.imgWit1.Size = new System.Drawing.Size(50, 50);
+            this.imgWit1.TabIndex = 1;
+            this.imgWit1.TabStop = false;
+            // 
+            // imgWit2
+            // 
+            this.imgWit2.Location = new System.Drawing.Point(1060, 599);
+            this.imgWit2.Name = "imgWit2";
+            this.imgWit2.Size = new System.Drawing.Size(50, 50);
+            this.imgWit2.TabIndex = 2;
+            this.imgWit2.TabStop = false;
+            // 
+            // imgGeel
+            // 
+            this.imgGeel.Location = new System.Drawing.Point(1116, 599);
+            this.imgGeel.Name = "imgGeel";
+            this.imgGeel.Size = new System.Drawing.Size(50, 50);
+            this.imgGeel.TabIndex = 4;
+            this.imgGeel.TabStop = false;
+            // 
+            // imgGroen
+            // 
+            this.imgGroen.Location = new System.Drawing.Point(1116, 543);
+            this.imgGroen.Name = "imgGroen";
+            this.imgGroen.Size = new System.Drawing.Size(50, 50);
+            this.imgGroen.TabIndex = 3;
+            this.imgGroen.TabStop = false;
+            // 
+            // imgRood
+            // 
+            this.imgRood.Location = new System.Drawing.Point(1172, 599);
+            this.imgRood.Name = "imgRood";
+            this.imgRood.Size = new System.Drawing.Size(50, 50);
+            this.imgRood.TabIndex = 6;
+            this.imgRood.TabStop = false;
+            // 
+            // imgBlauw
+            // 
+            this.imgBlauw.Location = new System.Drawing.Point(1172, 543);
+            this.imgBlauw.Name = "imgBlauw";
+            this.imgBlauw.Size = new System.Drawing.Size(50, 50);
+            this.imgBlauw.TabIndex = 5;
+            this.imgBlauw.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1234, 661);
+            this.Controls.Add(this.imgRood);
+            this.Controls.Add(this.imgBlauw);
+            this.Controls.Add(this.imgGeel);
+            this.Controls.Add(this.imgGroen);
+            this.Controls.Add(this.imgWit2);
+            this.Controls.Add(this.imgWit1);
+            this.Controls.Add(this.btndobbelsteen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgWit1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgWit2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgGeel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgGroen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgRood)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBlauw)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btndobbelsteen;
+        private System.Windows.Forms.PictureBox imgWit1;
+        private System.Windows.Forms.PictureBox imgWit2;
+        private System.Windows.Forms.PictureBox imgGeel;
+        private System.Windows.Forms.PictureBox imgGroen;
+        private System.Windows.Forms.PictureBox imgRood;
+        private System.Windows.Forms.PictureBox imgBlauw;
+    }
+}
+
---
+++ Qwixx/Qwixx/Form1.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Qwixx
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            qwixxKaartMaken(arrayButtonKaart1, 1);
+            
+            // qwixxKaartMaken(arrayButtonKaart2, 12);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = new System.Drawing.Size(1250 , 700);
+        }
+
+        // meervoudige array van buttons maken.
+        Button[,] arrayButtonKaart1 = new Button[4, 12];
+        // Button[,] arrayButtonKaart2 = new Button[4, 12];
+
+        // array true or false 
+        bool rood = true;
+        bool geel = true;
+        bool groen = true;
+        bool blauw = true; 
+
+        bool[,] arrayControl = new bool[4,12];
+
+
+        Random r = new Random();
+
+        // 6 dobbelstenen onthouden
+        int dobbelsteenW1, dobbelsteenW2, dobbelsteenGroen, dobbelsteenGeel, dobbelsteenBlauw, dobbelsteenRood;
+
+        // Methode voor Qwixx kaart.
+        void qwixxKaartMaken(Button [,] arrayButtonKaart, int xPositie)
+        {
+            int y = 1;
+
+            // buttons in een meervoudig Array
+
+            for (int rij = 0; rij < 4; rij++)
+            {
+                for (int kolom = 0; kolom < 12; kolom++)
+                {
+                    arrayButtonKaart[rij, kolom] = new Button();
+                    Controls.Add(arrayButtonKaart[rij, kolom]);
+
+                    switch (rij)
+                    {
+                        case 0:
+                            arrayButtonKaart[rij, kolom].BackColor = Color.Red;
+                            arrayButtonKaart[rij, kolom].Text = (kolom + 2).ToString();
+                            break;
+
+                        case 1:
+                            arrayButtonKaart[rij, kolom].BackColor = Color.Yellow;
+                            arrayButtonKaart[rij, kolom].Text = (kolom + 2).ToString();
+                            break;
+
+                        case 2:
+                            arrayButtonKaart[rij, kolom].BackColor = Color.LimeGreen;
+                            arrayButtonKaart[rij, kolom].Text = (-kolom + 12).ToString();
+                            break;
+
+                        case 3:
+                            arrayButtonKaart[rij, kolom].BackColor = Color.DodgerBlue;
+                            arrayButtonKaart[rij, kolom].Text = (-kolom + 12).ToString();
+                            break;
+
+                    }
+
+                    arrayButtonKaart[rij, kolom].TextAlign = ContentAlignment.MiddleCenter;
+                    arrayButtonKaart[rij, kolom].Size = new Size(40, 40);
+                    arrayButtonKaart[rij, kolom].Location = new Point((kolom + xPositie) * 50, (rij + y) * 50);
+                    arrayButtonKaart[rij, kolom].Click += ButtonOnClick;
+
+                    this.Controls.Add(arrayButtonKaart[rij, kolom]);
+
+                }
+            }
+
+
+
+            //plaatje op de laaste button 
+            for ( int i = 0; i < 4; i++)
+            {
+                arrayButtonKaart[i, 11].BackgroundImage = Image.FromFile(@"C:/Users/User/source/repos/Qwixx/images/lockopen.png");
+                arrayButtonKaart[i, 11].BackgroundImageLayout = ImageLayout.Stretch;
+            }
+
+
+        }
+        
+        // Dobbelsteen laten zien en rollen.
+        int dobbelsteenLatenzien(PictureBox pb, string kleur, bool aan)
+        {
+            int dobbelsteen;
+
+            dobbelsteen = r.Next(1, 7);
+            if (aan == true)
+            {
+                switch (dobbelsteen)
+                {
+
+                    case 1:
+                        pb.BackgroundImage = Image.FromFile(@"C:/Users/User/source/repos/Qwixx/images/dobbelstenen/dobbelsteen1" + kleur + ".png");
+                        pb.BackgroundImageLayout = ImageLayout.Stretch;
+                        break;
+
+                    case 2:
+                        pb.BackgroundImage = Image.FromFile(@"C:/Users/User/source/repos/Qwixx/images/dobbelstenen/dobbelsteen2" + kleur + ".png");
+                        pb.BackgroundImageLayout = ImageLayout.Stretch;
+                        break;
+
+                    case 3:
+                        pb.BackgroundImage = Image.FromFile(@"C:/Users/User/source/repos/Qwixx/images/dobbelstenen/dobbelsteen3" + kleur + ".png");
+                        pb.BackgroundImageLayout = ImageLayout.Stretch;
+                        break;
+
+                    case 4:
+                        pb.BackgroundImage = Image.FromFile(@"C:/Users/User/source/repos/Qwixx/images/dobbelstenen/dobbelsteen4" + kleur + ".png");
+                        pb.BackgroundImageLayout = ImageLayout.Stretch;
+                        break;
+
+                    case 5:
+                        pb.BackgroundImage = Image.FromFile(@"C:/Users/User/source/repos/Qwixx/images/dobbelstenen/dobbelsteen5" + kleur + ".png");
+                        pb.BackgroundImageLayout = ImageLayout.Stretch;
+                        break;
+
+                    case 6:
+                        pb.BackgroundImage = Image.FromFile(@"C:/Users/User/source/repos/Qwixx/images/dobbelstenen/dobbelsteen6" + kleur + ".png");
+                        pb.BackgroundImageLayout = ImageLayout.Stretch;
+                        break;
+                }
+
+               
+            }
+            else
+            {
+                pb.BackgroundImage = Image.FromFile(@"C:/Users/User/source/repos/Qwixx/images/crossout" + kleur + ".png");
+                pb.BackgroundImageLayout = ImageLayout.Stretch;
+                pb.Enabled = false;
+            }
+            return dobbelsteen;
+        }
+
+        private void ButtonOnClick(object sender, EventArgs eventArgs)
+        {
+            Button button = (Button) sender;
+            int intIndex;
+
+          
+
+            if (button.BackColor == Color.Red)
+            {
+                intIndex = int.Parse(button.Text) - 2;
+                if ( (dobbelsteenW1 + dobbelsteenW2) == intIndex + 2 || (dobbelsteenW1 + dobbelsteenRood == intIndex + 2) || (dobbelsteenW2 + dobbelsteenRood == intIndex + 2) )
+                {
+                    // Alle knoppen *voor* de gedrukte knop op uit zetten.
+                    // intIndex is gelijk aan de positie in the array van in de rode rij.
+                    arrayControl[0, intIndex] = true;
+                    for (int i = 0; i < intIndex; i++)
+                    {
+                       if (arrayControl[0, i] == false)
+                       {
+                         arrayButtonKaart1[0, i].Enabled = false;
+                         arrayButtonKaart1[0, i].BackColor = Color.Black;
+
+                       }
+                    }
+                        
+                    arrayButtonKaart1[0, intIndex].BackColor = Color.White;
+                    arrayControl[0, intIndex] = true;
+
+                    //Rij sluiten via de slot knop.
+                    if (intIndex == 10)
+                    {
+                        arrayButtonKaart1[0, 11].BackgroundImage = Image.FromFile(@"C:/Users/User/source/repos/Qwixx/images/lockclosed.png");
+                        arrayButtonKaart1[0, 11].BackgroundImageLayout = ImageLayout.Stretch;
+
+                        rood = false;
+                        imgRood.BackgroundImage = Image.FromFile(@"C:/Users/User/source/repos/Qwixx/images/crossoutrood.png");
+                    }
+                }
+                    
+                else 
+                {
+                  MessageBox.Show("Deze knop mag niet afgekruist worden.");
+                }
+                //Note: verbode vakjes worden nog steeds geteldt.
+            }
+
+            if (button.BackColor == Color.Yellow)
+            {
+                intIndex = int.Parse(button.Text) - 2;
+                if ((dobbelsteenW1 + dobbelsteenW2) == intIndex + 2 || (dobbelsteenW1 + dobbelsteenGeel == intIndex + 2) || (dobbelsteenW2 + dobbelsteenGeel == intIndex + 2))
+                {
+                    arrayControl[1, intIndex] = true;
+                    intIndex = int.Parse(button.Text) - 2;
+                    arrayControl[1, intIndex] = true;
+                    for (int i = 0; i < intIndex; i++)
+                    {
+                        if (arrayControl[1, i] == false)
+                        {
+                            arrayButtonKaart1[1, i].Enabled = false;
+                            arrayButtonKaart1[1, i].BackColor = Color.Black;
+
+                        }
+                    }
+                    
+                    arrayButtonKaart1[1, intIndex].BackColor = Color.White;
+                    arrayControl[1, intIndex] = true;
+
+                    //Rij sluiten via de slot knop.
+                    if (intIndex == 10)
+                    {
+                        arrayButtonKaart1[1, 11].BackgroundImage = Image.FromFile(@"C:/Users/User/source/repos/Qwixx/images/lockclosed.png");
+                        arrayButtonKaart1[1, 11].BackgroundImageLayout = ImageLayout.Stretch;
+
+                        geel = false;
+                        imgGeel.BackgroundImage = Image.FromFile(@"C:/Users/User/source/repos/Qwixx/images/crossoutgeel.png");
+                    }
+
+                    else
+                    {
+                        MessageBox.Show("Deze knop mag niet afgekruist worden.");
+                    }
+                }
+
+            }
+
+            if (button.BackColor == Color.LimeGreen)
+            {
+                intIndex = 12 - int.Parse(button.Text);
+                if ((dobbelsteenW1 + dobbelsteenW2) == intIndex || (dobbelsteenW1 + dobbelsteenGroen == intIndex ) || (dobbelsteenW2 + dobbelsteenGroen == intIndex ) )
+                {
+                    arrayControl[2, intIndex] = true;
+                    // De inhoud van intIndex is lengte van de array na het aftrekken van de cijfer in de knop die in de groene rij wordt ingedrukt.
+                    // Voorbeeld: 12 (lengte van de array) - 8
+                    intIndex = 12 - int.Parse(button.Text);
+                    arrayControl[2, intIndex] = true;
+                    for (int i = 0; i < intIndex; i++)
+                    {
+                        // ArrayControl kijkt als de knop wel of niet al is ingedrukt.
+                        if (arrayControl[2, i] == false)
+                        {
+                            arrayButtonKaart1[2, i].Enabled = false;
+                            arrayButtonKaart1[2, i].BackColor = Color.Black;
+                        }
+                    }
+                    
+                    arrayButtonKaart1[2, intIndex].BackColor = Color.White;
+                    arrayControl[2, intIndex] = true;
+
+                    //Rij sluiten via de slot knop.
+                    if (intIndex == 10)
+                    {
+                        arrayButtonKaart1[2, 11].BackgroundImage = Image.FromFile(@"C:/Users/User/source/repos/Qwixx/images/lockclosed.png");
+                        arrayButtonKaart1[2, 11].BackgroundImageLayout = ImageLayout.Stretch;
+
+                        groen = false;
+                        imgGroen.BackgroundImage = Image.FromFile(@"C:/Users/User/source/repos/Qwixx/images/crossoutgroen.png");
+                    }
+
+                    else
+                    {
+                        MessageBox.Show("Deze knop mag niet afgekruist worden.");
+                    }
+                    //Note: verbode vakjes worden nog steeds geteldt.
+                }
+
+            }
+
+            if (button.BackColor == Color.DodgerBlue)
+            {
+                // De inhoud van intIndex is lengte van de array na het aftrekken van de cijfer in de knop die in de groene rij wordt ingedrukt.
+                // Voorbeeld: 12 (lengte van de array) - 8
+                intIndex = 12 - int.Parse(button.Text);
+                if ( (dobbelsteenW1 + dobbelsteenW2) == intIndex + 2 || (dobbelsteenW1 + dobbelsteenBlauw == intIndex + 2) || (dobbelsteenW2 + dobbelsteenBlauw == intIndex + 2))
+                {
+                    arrayControl[3, intIndex] = true;
+                    intIndex = 12 - int.Parse(button.Text);
+                    arrayControl[3, intIndex] = true;
+                    for (int i = 0; i < intIndex; i++)
+                    {
+                        if (arrayControl[3, i] == false)
+                        {
+                            arrayButtonKaart1[3, i].Enabled = false;
+                            arrayButtonKaart1[3, i].BackColor = Color.Black;
+
+                        }
+                    }
+                    
+                    arrayButtonKaart1[3, intIndex].BackColor = Color.White;
+                    arrayControl[3, intIndex] = true;
+
+                }
+
+                if (intIndex == 10)
+                {
+                    arrayButtonKaart1[3, 11].BackgroundImage = Image.FromFile(@"C:/Users/User/source/repos/Qwixx/images/lockclosed.png");
+                    arrayButtonKaart1[3, 11].BackgroundImageLayout = ImageLayout.Stretch;
+
+                    // Bool die kijk als het waar is dat die bobbelsteen is uitgeschakeled en uitgeschakeled moet blijven.
+                    blauw = false;
+                    imgGroen.BackgroundImage = Image.FromFile(@"C:/Users/User/source/repos/Qwixx/images/crossoutblauw.png");
+                }
+
+                else
+                {
+                    MessageBox.Show("Deze knop mag niet afgekruist worden.");
+                }
+
+            }
+
+        }
+
+        
+        private void btndobbelsteen_Click(object sender, EventArgs e)
+        {
+            dobbelsteenW1 = dobbelsteenLatenzien(imgWit1, "wit", true);
+            dobbelsteenW2 = dobbelsteenLatenzien(imgWit2, "wit", true);
+            dobbelsteenGroen = dobbelsteenLatenzien(imgGroen, "groen", groen);
+            dobbelsteenGeel = dobbelsteenLatenzien(imgGeel, "geel", geel);
+            dobbelsteenBlauw = dobbelsteenLatenzien(imgBlauw, "blauw", blauw);
+            dobbelsteenRood = dobbelsteenLatenzien(imgRood, "rood", rood);
+        }
+    }
+}
